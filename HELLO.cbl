e257b0fa0dec85c05e395ab37b7ddd16e2768ad2
hfhbd <hfhbd@users.noreply.github.com>
2022-07-23 22:12:17 +0200

    Bump Gradle

--- HELLO.cbl
+++ HELLO.cbl
@@ -1,6 +1,21 @@
        IDENTIFICATION              DIVISION.
-       PROGRAM-ID.                 HELLO.
+      ******************************************************************
+       PROGRAM-ID.                 SECTIONS.
+       AUTHOR. WEDEMANN / Softwork.app
+       INSTALLATION. Softwork.app
+       DATE-WRITTEN TODAY.
+      ******************************************************************
        DATA                        DIVISION.
-       WORKING-STORAGE             SECTION.
+       WORKING-STORAGE SECTION.
+       77 WORLD PIC X(6) VALUE 'WORLD!'.
+       77 HELLO PIC X(6) VALUE 'HELLO'.
+      /*****************************************************************
        PROCEDURE                   DIVISION.
-           DISPLAY "HELLO WORLD!".
+      ******************************************************************
+           DISPLAY HELLO WORLD
+           PERFORM FOO.
+
+       FOO SECTION.
+      * Some Comment
+           MOVE "42" TO WORLD
+           DISPLAY "ANSWER"WORLD.
