e397809c6406fa45a776d205a53f5b075384c3ac
hfhbd <hfhbd@users.noreply.github.com>
2022-12-19 16:21:15 +0100

    Revert Hello

--- HELLO.cbl
+++ HELLO.cbl
@@ -1,21 +1,6 @@
        IDENTIFICATION              DIVISION.
-      ******************************************************************
-       PROGRAM-ID.                 SECTIONS.
-       AUTHOR. WEDEMANN / Softwork.app
-       INSTALLATION. Softwork.app
-       DATE-WRITTEN TODAY.
-      ******************************************************************
+       PROGRAM-ID.                 HELLO.
        DATA                        DIVISION.
-       WORKING-STORAGE SECTION.
-       77 WORLD PIC X(6) VALUE 'WORLD!'.
-       77 HELLO PIC X(6) VALUE 'HELLO'.
-      /*****************************************************************
+       WORKING-STORAGE             SECTION.
        PROCEDURE                   DIVISION.
-      ******************************************************************
-           DISPLAY HELLO WORLD
-           PERFORM FOO.
-
-       FOO SECTION.
-      * Some Comment
-           MOVE "42" TO WORLD
-           DISPLAY "ANSWER"WORLD.
+           DISPLAY "HELLO WORLD!".
